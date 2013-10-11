46a0657d9007ea25037e3ce2e6f199ce670c43b9
Evan Tahler <evan@taskrabbit.com>
2013-10-11 14:34:25 -0700

    worker tests

--- test/worker.js
+++ test/worker.js
@@ -2,10 +2,18 @@ describe('worker', function(){
 
   var specHelper = require(__dirname + "/_specHelper.js").specHelper;
   var should = require('should');
+  var os = require('os');
   var worker;
   var queue;
 
-  var jobs = {};
+  var jobs = {
+    "add": {
+      perform: function(a,b,callback){
+        var answer = a + b; 
+        callback(answer);
+      },
+    } 
+  };
   
   before(function(done){
     specHelper.connect(function(){
@@ -42,11 +50,82 @@ describe('worker', function(){
     });
   });
 
-  it('can clear previously crashed workers from the same host')
-  it('can work a job')
-  it('will return job responses as events on success')
-  it('will not work jobs that are not defined')
-  it('will place failed jobs in the failed list')
-  it('will catch exceptions within jobs and place the job in the failed list')
+  describe('crashing workers', function(){
+
+    it('can clear previously crashed workers from the same host', function(done){
+      var name1 = os.hostname() + ":" + "000" // fake pid
+      var name2 = os.hostname() + ":" + process.pid // fake pid
+      worker1 = new specHelper.AR.worker({connection: specHelper.connectionDetails, timeout: specHelper.timeout, name: name1}, jobs, function(){
+        worker1.init(function(){
+          worker1.running = false;
+          setTimeout(function(){
+            worker2 = new specHelper.AR.worker({connection: specHelper.connectionDetails, timeout: specHelper.timeout, name: name2}, jobs, function(){
+              worker2.on('cleaning_worker', function(worker, pid){
+                worker.should.equal(name1);
+                done();
+              });
+              worker2.workerCleanup();
+            });
+          }, 500);
+        });
+      });    
+    });
+
+  });
+
+  describe('integration', function(){
+
+    beforeEach(function(done){
+      worker = new specHelper.AR.worker({connection: specHelper.connectionDetails, timeout: specHelper.timeout, queues: specHelper.queue}, jobs, function(){
+        done();
+      });
+    });
+
+    afterEach(function(done){
+      worker.end(function(){
+        done();
+      });
+    });
+
+    it('can work a job and return succesfull things', function(done){
+      var listener = worker.on('success', function(q, job, result){
+        q.should.equal(specHelper.queue);
+        job.class.should.equal('add');
+        result.should.equal(3);
+
+        worker.removeAllListeners('success');
+        done();
+      })
+
+      queue.enqueue(specHelper.queue, "add", [1,2]);
+      worker.start();
+    });
+
+    it('will not work jobs that are not defined', function(done){
+      var listener = worker.on('error', function(q, job, error){
+        q.should.equal(specHelper.queue);
+        String(error).should.equal == "Error: No job defined for class 'somethingFake'";
+
+        worker.removeAllListeners('error');
+        done();
+      })
+
+      queue.enqueue(specHelper.queue, "somethingFake", []);
+      worker.start();
+    });
+
+    it('will place failed jobs in the failed queue', function(done){
+      specHelper.redis.rpop(specHelper.namespace + ":" + "failed", function(err, data){
+        data = JSON.parse(data);
+        data.queue.should.equal(specHelper.queue);
+        data.exception.should.equal('Error');
+        data.error.should.equal('No job defined for class \'somethingFake\'');
+
+        done();
+      })
+    });
+
+
+  });  
 
 });
