17915a02a86bce2fcc67a2d487620f974c615c39
Evan Tahler <evan@taskrabbit.com>
2013-12-27 16:13:05 -0800

    remove resque-bus

--- index.js
+++ index.js
@@ -2,4 +2,3 @@ exports.connection     = require(__dirname + "/lib/connection.js").connection;
 exports.queue          = require(__dirname + "/lib/queue.js").queue;
 exports.worker         = require(__dirname + "/lib/worker.js").worker;
 exports.scheduler      = require(__dirname + "/lib/scheduler.js").scheduler;
-exports.bus            = require(__dirname + "/lib/bus.js").bus;
