3c6cf72409413b3f460e214ae05fde2850c842d3
Evan Tahler <evan@taskrabbit.com>
2014-03-20 10:59:44 -0700

    simple worker and scheduler can emit error

--- examples/example.js
+++ examples/example.js
@@ -84,8 +84,9 @@ worker.on('pause',           function(){ console.log("worker paused"); })
 scheduler.on('start',             function(){ console.log("scheduler started"); })
 scheduler.on('end',               function(){ console.log("scheduler ended"); })
 scheduler.on('poll',              function(){ console.log("scheduler polling"); })
+scheduler.on('error',             function(error){ console.log("scheduler error >> " + error); })
 scheduler.on('working_timestamp', function(timestamp){ console.log("scheduler working timestamp " + timestamp); })
-scheduler.on('transferred_job',    function(timestamp, job){ console.log("scheduler enquing job " + timestamp + " >> " + JSON.stringify(job)); })
+scheduler.on('transferred_job',   function(timestamp, job){ console.log("scheduler enquing job " + timestamp + " >> " + JSON.stringify(job)); })
 
 ////////////////////////
 // CONNECT TO A QUEUE //
---
+++ examples/simpleWorker.js
@@ -0,0 +1,53 @@
+/////////////////////////
+// REQUIRE THE PACKAGE //
+/////////////////////////
+
+var NR = require(__dirname + "/../index.js");
+// In your projects: var NR = require("node-resque");
+
+///////////////////////////
+// SET UP THE CONNECTION //
+///////////////////////////
+
+var connectionDetails = {
+  host:      "127.0.0.1",
+  password:  "",
+  port:      6379,
+  database:  0,
+  // namespace: "resque",
+  // looping: true
+}
+
+////////////////////
+// START A WORKER //
+////////////////////
+
+var jobs = {
+  "add": {
+    plugins: [],
+    pluginOptions: {},
+    perform: function(a,b,callback){
+      console.log("got job");
+      callback(null, answer);
+    },
+  }
+};
+
+var worker = new NR.worker({connection: connectionDetails, queues: ['*']}, jobs, function(){
+  worker.workerCleanup(); // optional: cleanup any previous improperly shutdown workers
+  worker.start();
+});
+
+/////////////////////////
+// REGESTER FOR EVENTS //
+/////////////////////////
+
+worker.on('start',           function(){ console.log("worker started"); })
+worker.on('end',             function(){ console.log("worker ended"); })
+worker.on('cleaning_worker', function(worker, pid){ console.log("cleaning old worker " + worker); })
+worker.on('poll',            function(queue){ console.log("worker polling " + queue); })
+worker.on('job',             function(queue, job){ console.log("working job " + queue + " " + JSON.stringify(job)); })
+worker.on('reEnqueue',       function(queue, job, plugin){ console.log("reEnqueue job (" + plugin + ") " + queue + " " + JSON.stringify(job)); })
+worker.on('success',         function(queue, job, result){ console.log("job success " + queue + " " + JSON.stringify(job) + " >> " + result); })
+worker.on('error',           function(queue, job, error){ console.log("job failed " + queue + " " + JSON.stringify(job) + " >> " + error); })
+worker.on('pause',           function(){ console.log("worker paused"); })
