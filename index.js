b3ff300507c04999d598af57bab090fbfae37c26
Greg Coonrod <coogr02@ca.com>
2016-05-13 11:31:27 -0500

    Exposing the plugin runner.

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ exports.queue          = require(__dirname + "/lib/queue.js").queue;
 exports.worker         = require(__dirname + "/lib/worker.js").worker;
 exports.multiWorker    = require(__dirname + "/lib/multiWorker.js").multiWorker;
 exports.scheduler      = require(__dirname + "/lib/scheduler.js").scheduler;
+exports.pluginRunner   = require(__dirname + "/lib/pluginRunner.js");
