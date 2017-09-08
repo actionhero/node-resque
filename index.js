472ddb4819b02ac28e6302803ac329f977bd962c
Evan Tahler <evantahler@gmail.com>
2017-09-08 14:52:02 -0700

    scheduler tests passing

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ exports.Connection = require(path.join(__dirname, 'lib', 'connection.js')).Conne
 exports.Queue = require(path.join(__dirname, 'lib', 'queue.js')).Queue
 exports.Worker = require(path.join(__dirname, 'lib', 'worker.js')).Worker
 // exports.MultiWorker = require(path.join(__dirname, 'lib', 'multiWorker.js')).MultiWorker
-// exports.Scheduler = require(path.join(__dirname, 'lib', 'scheduler.js')).Scheduler
+exports.Scheduler = require(path.join(__dirname, 'lib', 'scheduler.js')).Scheduler
 exports.PluginRunner = require(path.join(__dirname, 'lib', 'pluginRunner.js'))
