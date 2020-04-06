6241bf40f00341d84fe8942ac58ba14b632a8ea6
Elan Ruusamäe <glen@delfi.ee>
2020-04-06 11:38:28 +0300

    Add inline documentation to addSubscriber method

--- src/utils/SubscribingEventEmitter.ts
+++ src/utils/SubscribingEventEmitter.ts
@@ -5,6 +5,20 @@ import { EventEmitter } from "events";
  * @see https://nodejs.org/api/events.html
  */
 export class SubscribingEventEmitter extends EventEmitter {
+  /**
+   * Add listeners to current class from a class "subscriber".
+   *
+   * The class must provide getSubscribedEvents() method
+   * that returns array of event name and listener pairs:
+   *
+   * getSubscribedEvents() {
+   *   return {
+   *      // this will subscribe to "example" event
+   *      "example": () => console.log("handler for exaple event called"),
+   *   };
+   * }
+   *
+   */
   async addSubscriber(subscriber: any) {
     let subscribedEvents = subscriber.getSubscribedEvents();
     for (let [event, listener] of Object.entries(subscribedEvents)) {
