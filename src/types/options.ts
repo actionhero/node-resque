c490cf6e8ef4c76b6343f9660f3a2b247b0807dd
Elan Ruusamäe <glen@delfi.ee>
2020-09-08 11:51:14 +0300

    Add support for IORedis.Cluster type in ConnectionOptions for TypeScript

--- src/types/options.ts
+++ src/types/options.ts
@@ -10,7 +10,7 @@ export interface ConnectionOptions {
   namespace?: string;
   looping?: boolean;
   options?: any;
-  redis?: IORedis.Redis;
+  redis?: IORedis.Redis | IORedis.Cluster;
   scanCount?: number;
 }
 
