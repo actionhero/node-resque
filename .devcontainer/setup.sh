122e45fe1e93252d207f102b7ce9a3b7219eb8bc
Evan Tahler <evan@delicioushat.com>
2021-06-22 15:05:29 +0000

    Update devcontainer script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,5 +2,10 @@
 
 echo "--- CONFIURING CODESPACE ---"
 
-sudo apt-get install redis-tools
+# configure node
+nvm install v16
+npm install
+
+# configure redis
+sudo apt-get install redis-tools -y
 docker run -p 6379:6379 --name redis -d redis
